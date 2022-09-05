80851a0ced3adf66df785463cf4f1812c30cac99
viniciuslunardi <vinilunardi00@gmail.com>
2022-09-05 19:42:01 -0300

    finishing tutorial

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import ContactListScreen from './ContactList'
+import ContactDetailsScreen from './ContactDetails'
 
 import HomeScreen from './Home'
 
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
+  ContactList: {screen: ContactListScreen},
+  ContactDetails: {screen: ContactDetailsScreen},
 });
  
 const App = createAppContainer(MainNavigator);
